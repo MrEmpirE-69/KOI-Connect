cff3d496497deb6ae46b243bf8296954b10c1583
MrEmpirE69 <kushal77nepal@gmail.com>
2025-04-11 01:24:41 +1000

    hello

--- client/src/pages/ClientPage/ClientDashboard.jsx
+++ client/src/pages/ClientPage/ClientDashboard.jsx
@@ -104,7 +104,7 @@ const ClientDashboard = () => {
 
         {/* Quick Stats */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
-          {[ 
+          {[
             {
               label: "Total Reviews",
               value: feedbackHistory.length,
